7017f1a284b568e72e418cc5d787c0866f33c8c5
heimesh <heimesh79@gmail.com>
2021-12-19 16:44:14 +0530

    Fixed links in HTML and made first js func

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
 function computerPlay(){
-    let computerChoice
     let x = Math.floor((Math.random() * 3) +1)
+
+    let computerChoice
     
+    if(x === 1){
+        computerChoice = "Rock"
+    }
+    else if (x === 2){
+        computerChoice = "Paper"
+    }
+    else {
+        computerChoice = "Scissors"
+    }
+    return computerChoice
 }
+

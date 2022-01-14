381a1951e7a8224008772cacf5b9765dfb72481b
heimesh <heimesh79@gmail.com>
2022-01-14 22:22:26 +0530

    Added scores and rounds

--- javascript.js
+++ javascript.js
@@ -15,3 +15,35 @@ function computerPlay(){
     return computerChoice
 }
 
+let playerscore = 0;
+let pcscore = 0;
+
+function playRound(playerSelection,computerSelection){
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if(computerSelection == playerSelection){
+        console.log("It's a tie!");
+    }
+    else if((computerSelection == "rock" && playerSelection == "paper") || (computerSelection == "paper" && playerSelection == "scissors") || (computerSelection == "scissors" && playerSelection == "rock")){
+        console.log("You win!");
+        playerscore++;
+    }
+    else if((computerSelection == "rock" && playerSelection == "scissors") || (computerSelection == "paper" && playerSelection == "rock") || (computerSelection == "scissors" && playerSelection == "paper")){
+        console.log("Computer Wins!");
+        pcscore++;
+    }
+};
+
+function game(){
+    let playerInput = "";
+    for(let i = 1; i <= 5; i++){
+        playerInput = prompt("sup?");
+        playRound(playerInput,computerPlay());
+    };
+ console.log("Pc score:", pcscore);
+ console.log("Player score:", playerscore);
+};
+
+console.log(game());
